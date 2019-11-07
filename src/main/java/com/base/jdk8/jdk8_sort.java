009ce675af47c80b679113bef72edc921160fdf3
liupeng24 <liupeng24@58.com>
2019-11-07 10:54:17 +0800

    增加jdk1.8特性

--- src/main/java/com/base/jdk8/jdk8_sort.java
+++ src/main/java/com/base/jdk8/jdk8_sort.java
@@ -1,8 +1,11 @@
 package com.base.jdk8;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * @author LiuPeng
@@ -18,21 +21,37 @@ public class jdk8_sort {
                 "Tomas Berdych", "Juan Martin Del Potro",
                 "Richard Gasquet", "John Isner"};
 
-       /* Arrays.sort(players, new Comparator<String>() {
+        Arrays.sort(players, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
                 return (o1.compareTo(o2));
             }
-        });*/
+        });
 
-        Comparator<String> comparator = (String s1, String s2) -> (s1.compareTo(s2));
-        Arrays.sort(players, comparator);
-        // 简化 Arrays.sort(players, (String s1, String s2) -> (s1.compareTo(s2)));
+        /*Comparator<String> comparator = (String s1, String s2) -> (s1.compareTo(s2));
+        Arrays.sort(players, comparator);*/
+        // 简化
+        Arrays.sort(players, (String s1, String s2) -> (s1.compareTo(s2)));
 
         List<String> list = Arrays.asList(players);
         System.out.println("-------------------------");
         list.forEach(System.out :: println);
         System.out.println("-------------------------");
         list.forEach((l) -> System.out.println(l + ";"));
+
+       // 使用lamda遍历list获取list下标的两种方式
+        /*List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+        Stream.iterate(0, i -> i + 1).limit(list.size()).forEach(i -> {
+            System.out.println(list.get(i));
+        });
+        IntStream.range(0,list.size()).forEach(i->{
+            System.out.println(list.get(i));
+        });*/
+
     }
 }
--- src/test/java/com/base/TestMap.java
+++ src/test/java/com/base/TestMap.java
@@ -21,9 +21,16 @@ public class TestMap {
         map.put("7", 7);
         map.put("4a", 4);
         map.put("6", 6);
+        map.put("21", 2);
+        map.put("31", 3);
+        map.put("71", 7);
+        map.put("4a1", 4);
+        map.put("61", 6);
         map.put("5a", 5);
+        map.get("6a");
+        /*System.out.println(1);
         Set<String> strings = map.keySet();
-        strings.stream().forEach(x -> System.out.println(x + ":" + map.get(x)));
+        strings.stream().forEach(x -> System.out.println(x + ":" + map.get(x)));*/
 
     }
 }
